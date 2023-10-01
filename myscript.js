492506b0ca23c1202a8dd9a21a188e6087ae1f39
RRealJ <renatorealjunior2@gmail.com>
2023-10-01 15:34:40 -0300

    Boas praticas, remodelagem

--- myscript.js
+++ myscript.js
@@ -1,9 +1,14 @@
-var controleCampos = 1;
 function addCampos(){
-    //pensando
+    var div = document.getElementById('formBody');
+    var linha = '<label>Nome</label><input type="text" name="nome[]" placeholder="Digite seu Nome"><label>CPF</label><input type="text" name="cpf[]" placeholder="Digite seu CPF"><label>Data de Nascimento</label><input type="text" name="dtNascimento[]" placeholder="Digite a Data"><br><br><label>Endereço</label><input type="text" name="endereco_logradouro[]" placeholder="Digite o Logradouro"><input type="text" name="endereco_numero[]" placeholder="Digite o Número"><input type="text" name="endereco_bairro[]" placeholder="Digite o Bairro"><input type="text" name="endereco_cidade[]" placeholder="Digite a Cidade"><button type="button" class="btn btn-outline-secondary" name="addCampos" onclick="addCampos()"> + </button>';
+    div.innerHTML += linha;
 }
 
 
-function gerarCurriculo(){
-    //pensando
+function validate(){
+    if (document.formulario.nome.value.length < 3){
+        alert("Escreva mais");
+        return false;
+    }
+    return true;
 }
---
+++ window_print.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Imprimindo uma Página</title>
+	<style type="text/css">
+		.cor-vermelha{
+			color: red;
+		}
+	</style>
+
+	<script type="text/javascript">
+		function imprimir(){
+			window.print();
+		}
+	</script>
+    
+</head>
+<body>
+	<h1>Título da Página</h1>
+	<p>Descrição longa em campo paragráfo.</p>
+	<h2>Novo destaque</h2>
+	<p class="cor-vermelha">
+		Descrição vinda do formulário: 
+		<?php 
+			if(isset($_POST['enviado'])){
+				foreach($_POST['enviado'] as $env){
+					echo "$env<br>";
+				}
+			}
+		?>
+	</p>
+	<button onclick="imprimir()">Imprimir</button>
+</body>
+</html>
+
