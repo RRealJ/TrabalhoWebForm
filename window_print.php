f72cd68f9bde00ed6fa3c7fcb858679daefb8923
RRealJ <renatorealjunior2@gmail.com>
2023-10-07 15:55:55 -0300

    Update PHP e Layout
    Colocado layout mais organizado, para melhor visualização das informações, também realizado filtro pra imprimir somente div do curriculo.

--- window_print.php
+++ window_print.php
@@ -4,77 +4,73 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Gerando Curriculo</title>
+
 	<style type="text/css">
-		.cor-vermelha{
-			color: red;
+		.Curriculo{
+			border-left: 4mm ridge rgb(211, 220, 50);
+			border-right: 2px solid rgb(130,136,30);
+			border-top: 2px solid rgb(130,136,30);
+			border-bottom: 2px solid rgb(130,136,30);
+			background-color: #ffffff;
+			width: 95%;
+			font-size: 22px;
+			padding: 10px 0px 10px 10px;
 		}
+
+		@media print {
+			body * {
+				visibility: hidden;
+			}
+			#printable, #printable * {
+				visibility: visible;
+			}
+			#printable {
+				position: fixed;
+				left: 0;
+				top: 0;
+			}		
+		}
+
 	</style>
+
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 	<script src="myscript.js"></script>  
 </head>
 
-<?php 
-function quebrarLinha($texto) {
-
-	$palavrasMax = 30;
-	$textoFormatado = "";
-	$i = 0;
-	$palavras = explode(" ", $texto);
-
-	foreach ($palavras as $p) {
-		$textoFormatado = $textoFormatado . " " . $p;
-		$i++;
-
-		if ($i == $palavrasMax){
-			echo "$textoFormatado<br>";
-			$textoFormatado = "";	
-			$i = 0;
-		}
-	}
-}
-
-?>
-
 <body>
 	<h1>Gerador de Curriculo</h1>
-	<p>Descrição longa em campo paragráfo.</p>
-	<h2>Novo destaque</h2>
-	<p class="cor-vermelha">
-		Descrição vinda do formulário: 
+		<p>
+			<?php 
+			echo '<div class="Curriculo" id=printable>';
+			$nome = $_POST['nome'];
+			$cpf = $_POST['cpf'];
+			$dtNascimento = $_POST['dtNascimento'];
+			$endLogra = $_POST['enderecoLogradouro'];
+			$endNumero = $_POST['enderecoNumero'];
+			$endBairro = $_POST['enderecoBairro'];
+			$endCidade = $_POST['enderecoCidade'];
 
-		<?php 
-		$nome = $_POST['nome'];
-		$cpf = $_POST['cpf'];
-		$dtNascimento = $_POST['dtNascimento'];
-		$endLogra = $_POST['enderecoLogradouro'];
-		$endNumero = $_POST['enderecoNumero'];
-		$endBairro = $_POST['enderecoBairro'];
-		$endCidade = $_POST['enderecoCidade'];
+			if(isset($nome)){
+				echo "Nome: $nome<br>";
+				echo "CPF: $cpf<br>";
+				echo "Data de Nascimento: $dtNascimento<br>";
+				echo "Endereço: $endLogra, $endNumero, $endBairro - $endCidade<br>";
+				echo "-----------------------------------------------------------------------------------------<br>";
+				echo "Experiências: <br><br>";
 
-		if(isset($nome)){
-			echo "<br>----------------------------------------------------------------------------------------------------------------<br>";
-			echo "Nome: $nome<br>";
-			echo "CPF: $cpf<br>";
-			echo "Data de Nascimento: $dtNascimento<br>";
-			echo "Endereço: $endLogra, $endNumero, $endBairro - $endCidade<br>";
-			echo "----------------------------------------------------------------------------------------------------------------<br>";
-			echo "Experiências: <br><br>";
-
-			if(isset($_POST['experiencias'])){
-				$experiencias = $_POST['experiencias'];
-				foreach ($experiencias as $exp) {
-					echo "->";
-					quebrarLinha($exp);
-					echo "<-<br><br>";
+				if(isset($_POST['experiencias'])){
+					$experiencias = $_POST['experiencias'];
+					foreach ($experiencias as $exp) {
+						$expFormatado = wordwrap($exp, 100, "<br />\n");
+						echo "$expFormatado<br><br>";
+					}
 				}
-			}	
-			echo "----------------------------------------------------------------------------------------------------------------<br>";
-		}
+			}
+			echo '</div>';
+			?>
+		</p>
+		<button onclick="imprimir()">Imprimir</button>
 
-		?>
-		
-	</p>
-	<button onclick="imprimir()">Imprimir</button>
 </body>
 </html>
 
